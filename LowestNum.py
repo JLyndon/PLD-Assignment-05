1e9a61fd62deb8a03e8aa0953c872cff70066f74
DESKTOP-VM4D9GJ <jolyndonjr@gmail.com>
2021-11-14 01:18:28 +0800

    Code Completed - Added Instructions for NegativeInpt

--- LowestNum.py
+++ LowestNum.py
@@ -5,30 +5,36 @@ def CommaReader_Single(StringVal): #Remodeled version of previous CommaReader co
     else:
         return StringVal
 
-def Validation(UsrInput):
+def Validation(UsrInput): #Input Validation Statements
     if UsrInput.isalpha() == True:
         return print("Input must be a number.")
     elif UsrInput.isdigit() == True:
         return print("Input must be in numerical form.")
     elif UsrInput.isalnum() == True:
         return print("Input must be only numbers.")
+    elif (UsrInput == "") or (UsrInput == None):
+        return print("Please enter a number.")
+    elif UsrInput.isspace() == True:
+        return print("Please fill all the blanks.")
+
 
 def Usr_inpt():
     while True: #Added conditions for input validation. Accepts decimal inputs.
         while True:
             Numb01 = input("\nEnter first number: ") #Accepts 'with comma' inputs. 
             DCNumb01 = CommaReader_Single(Numb01) #Checks for comma and omit for further processing.
-            if DCNumb01.replace(".","").isdecimal() == True: 
+            
+            if DCNumb01.replace(".","").replace("-","").isdecimal() == True: 
                 Fnl_01 = float(DCNumb01)
                 while True:
                     Numb02 = input("\nEnter second number: ")
                     DCNumb02 = CommaReader_Single(Numb02)
-                    if DCNumb02.replace(".","").isdecimal() == True:
+                    if DCNumb02.replace(".","").replace("-","").isdecimal() == True:
                         Fnl_02 = float(DCNumb02)
                         while True:
                             Numb03 = input("\nEnter third number: ")
                             DCNumb03 = CommaReader_Single(Numb03)
-                            if DCNumb03.replace(".","").isdecimal() == True:
+                            if DCNumb03.replace(".","").replace("-","").isdecimal() == True:
                                 Fnl_03 = float(DCNumb03)
                                 return Fnl_01, Fnl_02, Fnl_03
                             else:
@@ -61,7 +67,7 @@ LowestVal = str(Get_lowest_(First, Second, Third))
 wholeVal, fractionVal = LowestVal.split(".") #Splits the string into two.
 
 if int(fractionVal) == 0: #Statements that evaluate the eligibility for display.
-    if len(wholeVal) >= 4:
+    if (wholeVal.replace("-","").__len__()) >= 4:
         print(f"\n{wholeVal:,}")
     else:
         print(f"\n{wholeVal}")
