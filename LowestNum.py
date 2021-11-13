1bfa36e6b4a8ba516b2e2e6a1399a638d83439c7
DESKTOP-VM4D9GJ <jolyndonjr@gmail.com>
2021-11-13 20:00:06 +0800

    First Commit - Program B Input Statements

---
+++ LowestNum.py
@@ -0,0 +1,8 @@
+def usr_inputs():
+    Numb01 = int(input("Enter first number: "))
+    Numb02 = int(input("Enter second number: "))
+    Numb03 = int(input("Enter third number: "))
+    return Numb01, Numb02, Numb03
+
+Numbers = usr_inputs()
+print(Numbers)
