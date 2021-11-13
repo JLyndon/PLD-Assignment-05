743e0e6bbb26f30130845b4001f8d592a402d5e5
DESKTOP-VM4D9GJ <jolyndonjr@gmail.com>
2021-11-13 22:32:40 +0800

    Defined Instructions for Input and GetLow Operator

--- LowestNum.py
+++ LowestNum.py
@@ -1,20 +1,26 @@
-Numb01 = int(input("Enter first number: "))
-Numb02 = int(input("Enter second number: "))
-Numb03 = int(input("Enter third number: "))
+def Usr_inpt():
+    Numb01 = int(input("Enter first number: "))
+    Numb02 = int(input("Enter second number: "))
+    Numb03 = int(input("Enter third number: "))
+    return Numb01, Numb02, Numb03
 
-#Accepts inputs only with unique values
-if (Numb01 < Numb02) and (Numb01 < Numb03):
-    print(Numb01)
-elif (Numb02 < Numb01) and (Numb02 < Numb03):
-    print(Numb02)
-elif (Numb03 < Numb01) and (Numb03 < Numb02):
-    print(Numb03)
-elif (Numb03 == Numb01) and (Numb03 == Numb02):
-    print(Numb01)
-else: #Accepts inputs with alike values and still solve for the lowest value
-    if (Numb01 == Numb02) or (Numb01 == Numb03):
-        print(Numb01)
-    elif (Numb02 == Numb01) or (Numb02 == Numb03):
-        print(Numb02)
-    elif (Numb03 == Numb01) or (Numb03 == Numb02):
-        print(Numb03)
+#Accepts inputs with int val
+def Get_lowest_(FNum, Snum, ThNum):
+    if (FNum < Snum) and (FNum < ThNum):
+        print(FNum)
+    elif (Snum < FNum) and (Snum < ThNum):
+        print(Snum)
+    elif (ThNum < FNum) and (ThNum < Snum):
+        print(ThNum)
+    elif (ThNum == FNum) and (ThNum == Snum):
+        print(FNum)
+    else: #Accepts inputs with alike values and still solve for the lowest value
+        if (FNum == Snum) or (FNum == ThNum):
+            print(FNum)
+        elif (Snum == FNum) or (Snum == ThNum):
+            print(Snum)
+        elif (ThNum == FNum) or (ThNum == Snum):
+            print(ThNum)
+
+First, Second, Third = Usr_inpt()
+Get_lowest_(First, Second, Third)
