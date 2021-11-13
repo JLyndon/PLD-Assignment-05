389dd4b6da0d60e22811f9546dad9a3a15068291
DESKTOP-VM4D9GJ <jolyndonjr@gmail.com>
2021-11-13 20:54:56 +0800

    Added if-else Statements

--- LowestNum.py
+++ LowestNum.py
@@ -1,8 +1,11 @@
-def usr_inputs():
-    Numb01 = int(input("Enter first number: "))
-    Numb02 = int(input("Enter second number: "))
-    Numb03 = int(input("Enter third number: "))
-    return Numb01, Numb02, Numb03
+Numb01 = int(input("Enter first number: "))
+Numb02 = int(input("Enter second number: "))
+Numb03 = int(input("Enter third number: "))
 
-Numbers = usr_inputs()
-print(Numbers)
+if (Numb01 < Numb02) and (Numb01 < Numb03):
+    print(Numb01)
+elif (Numb02 < Numb01) and (Numb02 < Numb03):
+    print(Numb02)
+elif (Numb03 < Numb01) and (Numb03 < Numb02):
+    print(Numb03)
+
