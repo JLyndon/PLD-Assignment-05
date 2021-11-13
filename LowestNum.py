30afdb31556983017a270cdd73fadf254ddca6b8
DESKTOP-VM4D9GJ <jolyndonjr@gmail.com>
2021-11-13 23:24:51 +0800

    Added Nested Loops for Continuous Prompt

--- LowestNum.py
+++ LowestNum.py
@@ -1,21 +1,26 @@
 def Usr_inpt():
     while True: #Added conditions for input validation. Accepts decimal inputs.
-        Numb01 = input("\nEnter first number: ")
-        if Numb01.replace(".","").isdigit() == True:
-            Fnl_01 = float(Numb01)
-            Numb02 = input("Enter second number: ")
-            if Numb02.replace(".","").isdigit() == True:
-                Fnl_02 = float(Numb02)
-                Numb03 = input("Enter third number: ")
-                if Numb03.replace(".","").isdigit() == True:
-                    Fnl_03 = float(Numb03)
-                    return Fnl_01, Fnl_02, Fnl_03
-                else:
-                    print("Enter valid characters.")
+        while True:
+            Numb01 = input("\nEnter first number: ")
+            if Numb01.replace(".","").isdigit() == True:
+                Fnl_01 = float(Numb01)
+                pass
+                while True:
+                    Numb02 = input("Enter second number: ")
+                    if Numb02.replace(".","").isdigit() == True:
+                        Fnl_02 = float(Numb02)
+                        pass
+                        while True:
+                            Numb03 = input("Enter third number: ")
+                            if Numb03.replace(".","").isdigit() == True:
+                                Fnl_03 = float(Numb03)
+                                return Fnl_01, Fnl_02, Fnl_03
+                            else:
+                                print("Enter valid characters.")
+                    else:
+                        print("Enter valid characters.")
             else:
                 print("Enter valid characters.")
-        else:
-            print("Enter valid characters.")
 
 def Get_lowest_(FNum, Snum, ThNum):
     if (FNum < Snum) and (FNum < ThNum):
