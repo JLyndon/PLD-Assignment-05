12579cb19a046abf39c54c0032e286e56fa9a643
DESKTOP-VM4D9GJ <jolyndonjr@gmail.com>
2021-11-14 18:47:30 +0800

    Added Periodic Reminder - Adjusted Conditions for Inputs

--- GrdSys.py
+++ GrdSys.py
@@ -36,40 +36,57 @@ def GradeClassifier(GradeReference):
         return print("Grade/Mark: 3.0\nDescription: Passing")
     elif GradeReference >= 65 and GradeReference <= 74:
         return print("Grade/Mark: 5.0\nDescription: Failure")
+    elif GradeReference > 100:
+        return print(f"Grades are capped at 100. Please enter values within range.")
     else:
-        return print(f"The rating is unavailable for the grade {init_grade}.")
+        return print(f"There's no available rating for the grade of '{init_grade}'")
 
 def UnivGradeClassifier(FlatGrade, Decimals):
-    if FlatGrade == "1":
-        if (Decimals == None) or (int(Decimals) == 0):
-            print("Percentage Range: 97-100\nDescription: Excellent")
-        elif Decimals == "25":
-            print("Percentage Range: 94-96\nDescription: Excellent")
-        elif (Decimals == "50") or (Decimals == "5"):
-            print("Percentage Range: 91-93\nDescription: Very Good")
-        elif Decimals == "75":
-            print("Percentage Range: 88-90\nDescription: Very Good")
-    elif FlatGrade == "2":
-        if (Decimals == None) or (int(Decimals) == 0):
-            print("Percentage Range: 85-89\nDescription: Good")
-        elif Decimals == "25":
-            print("Percentage Range: 79-81\nDescription: Good")
-        elif (Decimals == "50") or (Decimals == "5"):
-            print("Percentage Range: 76-78\nDescription: Satisfactory")
-        elif Decimals == "75":
-            print("Percentage Range: 76-78\nDescription: Satisfactory")
-    elif FlatGrade == "3":
-        if (Decimals == None) or (int(Decimals) == 0):
-            print("Percentage Range: 75\nDescription: Passing")
-    elif FlatGrade == "5":
-        if (Decimals == None) or (int(Decimals) == 0):
-            print("Percentage Range: 65-74\nDescription: Failure")
+    if (int(FlatGrade) >= 0) and (int(FlatGrade) <= 5):
+        if FlatGrade == "1":
+            if (Decimals == None) or (int(Decimals) == 0):
+                print("Percentage Range: 97-100\nDescription: Excellent")
+            elif Decimals == "25":
+                print("Percentage Range: 94-96\nDescription: Excellent")
+            elif (Decimals == "50") or (Decimals == "5"):
+                print("Percentage Range: 91-93\nDescription: Very Good")
+            elif Decimals == "75":
+                print("Percentage Range: 88-90\nDescription: Very Good")
+            else:
+                return print(f"There's no available rating for the grade of '{secon_grade}'")
+        elif FlatGrade == "2":
+            if (Decimals == None) or (int(Decimals) == 0):
+                print("Percentage Range: 85-89\nDescription: Good")
+            elif Decimals == "25":
+                print("Percentage Range: 79-81\nDescription: Good")
+            elif (Decimals == "50") or (Decimals == "5"):
+                print("Percentage Range: 76-78\nDescription: Satisfactory")
+            elif Decimals == "75":
+                print("Percentage Range: 76-78\nDescription: Satisfactory")
+            else:
+                return print(f"There's no available rating for the grade of '{secon_grade}'")
+        elif FlatGrade == "3":
+            if (Decimals == None) or (int(Decimals) == 0):
+                print("Percentage Range: 75\nDescription: Passing")
+            else:
+                return print(f"There's no available rating for the grade of '{secon_grade}'")
+        elif FlatGrade == "5":
+            if (Decimals == None) or (int(Decimals) == 0):
+                print("Percentage Range: 65-74\nDescription: Failure")
+            else:
+                return print(f"There's no available rating for the grade of '{secon_grade}'")
+    elif (int(FlatGrade) < 0) or (int(FlatGrade) > 5):
+        return print("The Grading System has its standard scale (1.0-3.0 & 5.0). Please, be guided accordingly.")
 
 def ValidationResponse(StringVal):
     if StringVal.lower() == "back":
         time.sleep(2)
         print("\n\nWelcome back to the Main Menu!\n\nPlease choose from the following actions:\nType\n1 - To check grades in scale of 1-100\n2 - To check grades in scale of 1.0-5.0\n3 - To view grades with specified Remarks\nexit - To exit the program")
         return "return_menu"
+    elif StringVal.replace("-","").replace(".","").isdigit() == True:
+        print("Grades are all positive. Please enter a postive value.")
+    elif StringVal.lower() == "exit":
+        print("To exit, type 'back' and return to the main menu.")
     elif StringVal.replace(".","").lower() == "inc":
         return print("Percentage Range: N/A\nDescription: Incomplete")
     elif StringVal.replace(".","").upper() == "W":
@@ -85,27 +102,36 @@ def ValidationResponse(StringVal):
 
 def DecisionMenu():
     global init_grade
+    global secon_grade
     print("Welcome to Grade Checker!\n\nPlease choose from the following actions:\nType\n1 - To check grades in scale of 1-100\n2 - To check grades in scale of 1.0-5.0\n3 - To view grades with specified Remarks\nexit - To exit the program")
+    TransactionNo = 0
+    TransactionLimit = 4
     while True:
         Usr_decision = input("\n> ")
         if Usr_decision == "1":
             print("Type 'back' - To return to the main menu")
             while True:
                 init_grade = input("\nEnter Grade: ")
-                if init_grade.replace(".","").isdecimal() == True:
-                    GradeClassifier(Rounder_(init_grade))
+                TransactionNo += 1
+                if TransactionNo == TransactionLimit:
+                    TransactionNo = 0
+                    TransactionLimit += 1
+                    print("\n\nType 'back' - To return to the main menu\nEnter a value - To proceed\n")
                 else:
-                    if (ValidationResponse(init_grade)) == "return_menu":
-                        break
+                    if init_grade.replace(".","").isdecimal() == True:
+                        GradeClassifier(Rounder_(init_grade))
                     else:
-                        pass
+                        if (ValidationResponse(init_grade)) == "return_menu":
+                            break
+                        else:
+                            pass
         elif Usr_decision == "2":
             print("Type 'back' - To return to the main menu")
             while True:
-                secon_grade = input("Enter Grade: ")
+                secon_grade = input("\nEnter Grade: ")
                 if secon_grade.replace(".","").isdecimal() == True:
                     if "." not in secon_grade:
-                        UnivGradeClassifier(secon_grade[-1], None)
+                        UnivGradeClassifier(secon_grade, None)
                     else:
                         Whole, Fraction = secon_grade.split(".")
                         UnivGradeClassifier(Whole[0], Fraction[0:2])
@@ -119,20 +145,7 @@ def DecisionMenu():
         elif Usr_decision.lower() == "exit":
             print("Thank you for stopping by! Have a great day :)")
             break
-    
-DecisionMenu()
+        else:
+            print("Please enter from the menu.")
 
-# program #2: grading
-# grade/mark    percentage     description
-# 1.0           97-100          Excellent
-# 1.25          94-96           Excellent
-# 1.5           91-93           Very Good
-# 1.75          88-90           Very Good
-# 2.0           85-87           Good
-# 2.25          82-84           Good
-# 2.75          76-78           Satisfactory
-# 3.0           75              Passing
-# 5.0           65-74           Failure
-# Inc.                          Incomplete
-# W                             Withdrawn
-# D                             Dropeed
+DecisionMenu()
