022841841aa435d2e8c0d85b33a05462e33faee4
DESKTOP-VM4D9GJ <jolyndonjr@gmail.com>
2021-11-15 11:45:33 +0800

    Defined Function for Determing Life Stage based on AgeInpt

--- StagesofLife.py
+++ StagesofLife.py
@@ -1,16 +1,17 @@
-age = int(input("Enter age: "))
-
-if (age >= 0) and (age <= 12):
-    print("Kid")
-else:
-    if (age >= 13) and (age <= 17):
-        print("Teen")
+def DetermineLifeStage(Usr_Age):
+    if (Usr_Age >= 0) and (Usr_Age <= 12):
+        print("Kid")
     else:
-        if age == 18:
-            print("Debut")
+        if (Usr_Age >= 13) and (Usr_Age <= 17):
+            print("Teen")
         else:
-            if age >= 19:
-                print("Adult")
+            if Usr_Age == 18:
+                print("Debut")
             else:
-                print("Enter a valid age.")
-                
+                if Usr_Age >= 19:
+                    print("Adult")
+                else:
+                    print("Enter a valid age.")
+
+age = int(input("Enter age: "))
+DetermineLifeStage(age)
